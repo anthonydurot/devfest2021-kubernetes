3a3ee8fb5ecde557601e1a6a0bc16c17c5534284
Anthony Durot <anthony.durot@ext.bpifrance.fr>
2021-11-13 18:59:06 +0100

    Sealed secret : Oubli service

--- vault/install.sh
+++ vault/install.sh
@@ -1,6 +1,3 @@
-kubectl create namespace vault
 helm repo add hashicorp https://helm.releases.hashicorp.com
-helm install vault hashicorp/vault \    
---namespace vault \    
---set "server.dev.enabled=true" \
---dry-run
+helm install vault hashicorp/vault --namespace vault -f values.yaml --create-namespace
+kubectl apply -f ingress.yaml
