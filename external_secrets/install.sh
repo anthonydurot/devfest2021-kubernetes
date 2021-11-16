a3566d7943aaab5f252ed12e0cee9aace2dc3346
Anthony Durot <anthony.durot@ext.bpifrance.fr>
2021-11-16 22:37:01 +0100

    WIP

--- external_secrets/install.sh
+++ external_secrets/install.sh
@@ -1,2 +1,2 @@
 helm repo add external-secrets https://external-secrets.github.io/kubernetes-external-secrets/
-helm install [RELEASE_NAME] external-secrets/kubernetes-external-secrets
+helm install external-secrets external-secrets/kubernetes-external-secrets --namespace external-secrets --create-namespace -f values.yaml
